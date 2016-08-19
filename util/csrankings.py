767afcd1eb14830089fce4fe8e11e542d310f1fe
Ryan Newton <rrnewton@gmail.com>
2016-08-19 10:33:37 -0400

    Revert "Add bioinformatics"
    This reverts commit 16732c9088574d65c6df16ed517364914e0ff93f.

--- util/csrankings.py
+++ util/csrankings.py
@@ -69,7 +69,6 @@ areadict = {
     'mobile' : ['MobiSys','MobiCom','MOBICOM','SenSys'],
     'robotics' : ['ICRA','IROS','Robotics: Science and Systems'],
     'crypto' : ['CRYPTO', 'CRYPTO (1)', 'CRYPTO (2)', 'EUROCRYPT', 'EUROCRYPT (1)', 'EUROCRYPT (2)']
-    'bioinfo' : [ 'RECOMB', 'ISMB' ] 
 }
 
 # Build a dictionary mapping conferences to areas.
